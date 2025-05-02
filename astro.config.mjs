a4cd5f3169da9179649e00e06ecbbc0bfdb56f75
danielgof <gofmandon@gmail.com>
2025-05-01 22:08:32 -0400

    new quizes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://danielgof/ai-welding.github.io',
+  base: 'https://github.com/danielgof/ai-welding.github.io',
+});

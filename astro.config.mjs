009be484606c043580728a83f465b8816a27f212
danielgof <gofmandon@gmail.com>
2025-05-01 22:13:13 -0400

    no suffix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ai-welding.github.io',
+  site: 'https://ai-welding',
   base: 'https://github.com/danielgof/ai-welding.github.io',
 });

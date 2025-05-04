0f3b25ad2899835dd5c5245a640a4042c346e217
danielgof <gofmandon@gmail.com>
2025-05-03 20:26:24 -0400

    fix base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ai-welding',
-  base: 'https://github.com/danielgof/ai-welding.github.io',
+  site: 'https://danielgof.github.io/ai-welding',
+  base: '/ai-welding/',
 });
